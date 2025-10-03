91ae8126e4ebbf8ad0e5f8817c0b5cbdb0904a9d
TomasKepic <tomas.kepic@hotmail.com>
2025-10-03 16:03:43 +0200

    Pridané Radzen Blazor komponenty

--- RUZWatcher/Program.cs
+++ RUZWatcher/Program.cs
@@ -1,3 +1,4 @@
+using Radzen;
 using RUZWatcher.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddRadzenComponents();
 
 var app = builder.Build();
 
